3b92daf8853120b50a5f90dcd2fba22bc37741ab
MohamedFarid2018 <mohamedamal680@gmail.com>
2018-12-31 12:58:19 +0200

    test ejabi

--- model/Need/schema.js
+++ model/Need/schema.js
@@ -1,7 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId
 
 const NeedSchema = new Schema({
 })
+
+const job_grouping = new Schema({
+    name: String,
+    job_titles: [
+        {
+            name: String,
+            JobTitle: {
+                type: ObjectId,
+                ref: "jobTitle",
+            },
+        },
+    ],
+    job_title_degree: Number,
+    entity: {
+        Entity: {
+            type: ObjectId,
+            ref: 'Organization', // created by Soliman
+        },
+        name: String,
+        level: Number,
+    },
+    target_level: String,
+})
+
 
 module.exports.NeedSchema = NeedSchema;
